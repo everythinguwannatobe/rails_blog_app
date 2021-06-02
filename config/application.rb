cfd728cd1f239ed33081a8724a3025fb3e1b346b
spencerduncanpower <timdonerpower@gmail.com>
2021-06-02 18:06:17 -0400

    Initial_commit

---
+++ config/application.rb
@@ -0,0 +1,31 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module RailsYourCongress
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.1
+
+    config.generators do |g|
+      g.stylesheets false
+      g.helper nil
+      g.test_framework :rspec,
+        fixtures:         false,
+        view_specs:       false,
+        helper_specs:     false,
+        routing_specs:    false,
+        request_specs:    false,
+        controller_specs: false
+    end
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+  end
+end
