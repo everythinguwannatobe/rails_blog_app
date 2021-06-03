31161ba9595a999cf4a08358789cf5f17a61be68
spencerduncanpower <timdonerpower@gmail.com>
2021-06-03 17:36:20 -0400

    Editing articles (#8)
    * Sined up
    * Edit article action and test
    * Edit article action and test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :resource_not_found
+
+  protected
+
+  def resource_not_found
+    message = "The article you are looking for could not be found"
+    flash[:alert] = message
+    redirect_to root_path
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,4 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
-
 end
