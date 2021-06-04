8dbedb0bbe07840b6986bde5293de700e3d61483
spencerduncanpower <timdonerpower@gmail.com>
2021-06-04 15:32:23 -0400

    Update show article spec

--- spec/system/show_articles_spec.rb
+++ spec/system/show_articles_spec.rb
@@ -3,18 +3,47 @@ require "rails_helper"
 RSpec.describe "ShowArticles", type: :system do
   before do
     @john = User.create(email: "john@example.com", password: "password")
+    @fred = User.create(email: "fred@example.com", password: "password")
     @article = Article.create(
       title: "First Article", body: "Lorem ipsum dolor sit amet, consectetur.", user: @john
     )
   end
 
-  scenario "A user shows an article" do
+  scenario "to non-signed in user hide the Edit and Delete buttons" do
     visit root_path
-
     click_link @article.title
 
     expect(page).to have_content(@article.title)
     expect(page).to have_content(@article.body)
-    expect(page).to have_current_path(article_path(@article), ignore_query: true)
+    expect(page).to have_current_path(article_path(@article))
+
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
+  end
+
+  scenario "to non-owner hide the Edit and Delete buttons" do
+    login_as(@fred)
+    visit "/"
+    click_link @article.title
+
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
+  end
+
+  scenario "A signed in owner sees both the Edit and Delete buttons" do
+    login_as(@john)
+    visit "/"
+    click_link @article.title
+
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+
+    expect(page).to have_link("Edit Article")
+    expect(page).to have_link("Delete Article")
   end
 end
