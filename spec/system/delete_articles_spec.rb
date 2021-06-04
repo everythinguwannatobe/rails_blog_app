b9b202583c7a8032b52c4fbe5d77f132ed19f63a
spencerduncanpower <timdonerpower@gmail.com>
2021-06-04 07:45:44 -0400

    Update Guardfile

--- spec/system/delete_articles_spec.rb
+++ spec/system/delete_articles_spec.rb
@@ -3,7 +3,8 @@ require "rails_helper"
 RSpec.describe "DeleteArticles", type: :system do
   before do
     @article = Article.create!(
-      title: "First Article", body: "Body of article one")
+      title: "First Article", body: "Body of article one"
+    )
   end
 
   scenario "User deletes an article" do
