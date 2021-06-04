bd217346ba690aff90162115766d695a127d3872
spencerduncanpower <timdonerpower@gmail.com>
2021-06-04 15:46:07 -0400

    Restrict access with controller actions

--- spec/request/articles_spec.rb
+++ spec/request/articles_spec.rb
@@ -3,15 +3,18 @@ require "rails_helper"
 RSpec.describe "Articles", type: :request do
   before do
     @john = User.create(email: "john@example.com", password: "password")
+    @fred = User.create(email: "fred@example.com", password: "password")
     @article = Article.create!(title: "Title One", body: "Body of article one", user: @john)
   end
 
-  describe "GET /articles/:id" do
-    context "with existing article" do
-      before { get "/articles/#{@article.id}" }
+  describe "GET /articles/:id/edit" do
+    context "with non-signed in user" do
+      before { get "/articles/#{@article.id}/edit" }
 
-      it "handles existing article" do
-        expect(response.status).to eq 200
+      it "redirects to the signin page" do
+        expect(response.status).to eq 302
+        flash_message = "You can only edit your own articles."
+        expect(flash[:alert]).to eq flash_message
       end
     end
 
@@ -22,6 +25,17 @@ RSpec.describe "Articles", type: :request do
         expect(response.status).to eq 302
         flash_message = "The article you are looking for could not be found"
         expect(flash[:alert]).to eq flash_message
+      end
+    end
+
+    context "with signed in user as owner" do
+      before do
+        login_as(@john)
+        get "/articles/#{@article.id}/edit"
+      end
+
+      it "successfully edits article" do
+        expect(response.status).to eq 200
       end
     end
   end
